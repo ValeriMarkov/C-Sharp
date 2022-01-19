b6784eb1a09bdc01640e0b1b3cdd4727543f8cbf
Valeri <96831672+ValeriMarkov@users.noreply.github.com>
2022-01-19 14:33:28 +0200

    Add files via upload

---
+++ Simple practices/One minus one/One minus one/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i += 2)
+            {
+                int j = i - (i * 2) + 1;
+                Write($"{j} {i} ");
+            }
+        }
+    }
+}
